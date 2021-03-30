b5aab10fca8311be1e2ccf2a8a4b7318e83857a7
saumya <saumya_11815073@nitkkr.ac.in>
2021-03-30 22:54:14 +0530

    modified app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { get } = require("http");
 const https = require("https");
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
