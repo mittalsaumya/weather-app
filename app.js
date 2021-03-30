d140cdcbd92ef4c5065359e01cce1a92009284c4
saumya <saumya_11815073@nitkkr.ac.in>
2021-03-30 21:23:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { get } = require("http");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+  
+});
+
+app.post("/",function(req,res){
+  var city=req.body.city_name;
+  const url="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7e79ad35793507dca32ae6c56d2abd7b"
+  https.get(url,function(response){
+		console.log(response.statusCode);
+		response.on("data",function(data){
+			const weatherData=JSON.parse(data);
+			const temp=weatherData.main.temp-273;
+			const weatherDes=weatherData.weather[0].description;
+			const icon=weatherData.weather[0].icon;
+			console.log(icon);
+			const imgurl="https://openweathermap.org/img/wn/" + icon + "@2x.png";
+			res.send("<h1>The temprature is " + temp + "  degree celcius</h1> <br> <h1>weather is " + weatherDes + " in " + city + " <img src=" + imgurl + "> </h1>");
+		})
+
+  });
+
+  //res.send("city is " + city);
+})
+
+app.get("/about",function(req,res){
+  res.send("saumya");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server");
+})
+
+//7e79ad35793507dca32ae6c56d2abd7b
+//https://samples.openweathermap.org/data/2.5/weather?q=london&appid=7e79ad35793507dca32ae6c56d2abd7b
